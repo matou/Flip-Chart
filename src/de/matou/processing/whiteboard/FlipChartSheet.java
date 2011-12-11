e4aa4be1aec8dbce0ed9ef2e82001ac03ed55a81
Matthias Matousek <m@tou.io>
2011-12-11 13:08:48 +0100

    added a sheet

---
+++ src/de/matou/processing/whiteboard/FlipChartSheet.java
@@ -0,0 +1,11 @@
+package de.matou.processing.flipchart;
+
+/**
+ * A flip chart sheet is unlimited in it's size and flip chart objects can be
+ * drawn upon it.
+ *
+ * @author Matthias Matousek
+ */
+class FlipChartSheet {
+}
+
