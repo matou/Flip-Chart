204ab51c0f34b605375ff6ac79bc2a842eddc353
Matthias Matousek <m@tou.io>
2011-12-11 02:39:47 +0100

    the implementation of the abstract whiteboard object

--- src/de/matou/processing/whiteboard/WhiteboardObject.java
+++ src/de/matou/processing/whiteboard/WhiteboardObject.java
@@ -1,7 +1,25 @@
+package de.matou.processing.whiteboard;
 
 abstract class WhiteboardObject {
 
     // coordinates
+    protected int posX = 0;
+    protected int posY = 0;
 
+    // the colors
+    protected int colorRed = 0;
+    protected int colorGreen = 0;
+    protected int colorBlue = 0;
+
+    /**
+     * Draws this whiteboard object to the given whiteboard at it's coordinates
+     * plus the given offset.
+     *
+     * @param board the whiteboard to draw to
+     * @param xOffset the offset to add to posX
+     * @param yOffset the offset to add to posY
+     */
+    abstract void draw(Whiteboard board, int xOffset, int yOffset);
+    
 }
 
