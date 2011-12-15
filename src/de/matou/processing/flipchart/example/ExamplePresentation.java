6729aea5f94501d649a64661863b7016c517a014
Matthias Matousek <m@tou.io>
2011-12-15 23:14:44 +0100

    an example presentation

---
+++ src/de/matou/processing/flipchart/example/ExamplePresentation.java
@@ -0,0 +1,49 @@
+package de.matou.processing.flipchart.example;
+
+import de.matou.processing.flipchart.FlipChartPresentation;
+import de.matou.processing.flipchart.FlipChartPresenter;
+import de.matou.processing.flipchart.FlipChartSheet;
+import de.matou.processing.flipchart.Rectangle;
+import de.matou.processing.flipchart.Text;
+
+public class ExamplePresentation extends FlipChartPresentation {
+
+    private FlipChartSheet current = null;
+    private boolean hasNext = false;
+
+    void start() {
+
+        new FlipChartPresenter().startPresentation(this);
+
+        current = new FlipChartSheet();
+        current.addObject(new Text("hello", 50, 50, 0));
+        hasNext = true;
+        stateChanged();
+
+        try {Thread.sleep(3000); } catch (Exception e) {}
+
+        current.addObject(new Text("world", 50, 70, 0));
+        current.addObject(new Rectangle(100, 100, 50, 50, 120));
+        hasNext = true;
+        stateChanged();
+
+
+    }
+
+    @Override
+    public FlipChartSheet getNextSheet() {
+        hasNext = false;
+        return current;
+    }
+
+    @Override
+    public boolean hasNextSheet() {
+        return hasNext;
+    }
+
+    public static void main(String[] args) {
+        new ExamplePresentation().start();
+    }
+
+}
+
